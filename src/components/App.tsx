9cc868fe11ab2e5044adfd36fd4725ed67a5a84f
A1DS19 <jp06171@gmail.com>
2021-01-05 23:00:59 -0600

    app y index tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import './css/app.css';
+import React, { Fragment, useState } from 'react';
+import { Container } from 'semantic-ui-react';
+import { EventDashboard } from './events/eventDashboard/EventDashboard';
+import { Navbar } from './nav/Navbar';
 
 const App = () => {
-  return <div>social</div>;
+  const [formOpen, setFormOpen] = useState(false);
+
+  return (
+    <Fragment>
+      <Navbar setFormOpen={setFormOpen} />
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </Fragment>
+  );
 };
 
 export { App };
