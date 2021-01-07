dde2935847ad525274aba1ad225f0bd0b7a34249
A1DS19 <jp06171@gmail.com>
2021-01-06 22:29:48 -0600

    navbar con auth

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
+import { SignedOutMenu } from './SignedOutMenu';
+import { SignedInMenu } from './SignedInMenu';
 
-interface NavbarProps {
-  setFormOpen: (state: boolean) => void;
-}
+const Navbar = (): JSX.Element => {
+  const [auth, setAuth] = useState(false);
+  const history = useHistory();
 
-const Navbar = ({ setFormOpen }: NavbarProps): JSX.Element => {
-  const showEventForm = () => setFormOpen(true);
+  const onSignOutPush = (): void => {
+    setAuth(false);
+    history.push('/');
+  };
 
   return (
     <Menu inverted fixed='top'>
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} exact to='/' header>
           <img src='/assets/logo.png' alt='logo' style={{ marginRight: '15px' }} />
           socialNET
         </Menu.Item>
-        <Menu.Item name='Events' />
-        <Menu.Item>
-          <Button onClick={showEventForm} positive inverted content='Crear Evento' />
-        </Menu.Item>
-        <Menu.Item position='right'>
-          <Button basic inverted content='Login' />
-          <Button basic inverted content='Registro' style={{ marginLeft: '0.5em' }} />
-        </Menu.Item>
+        <Menu.Item as={NavLink} to='/events' name='Events' />
+        {auth && (
+          <Menu.Item as={NavLink} to='/createEvent'>
+            <Button positive inverted content='Crear Evento' />
+          </Menu.Item>
+        )}
+        {auth ? (
+          <SignedInMenu onSignOutPush={onSignOutPush} />
+        ) : (
+          <SignedOutMenu setAuth={setAuth} />
+        )}
       </Container>
     </Menu>
   );
