dc923c483dfc422a075d0b4a80da979812e7174b
A1DS19 <jp06171@gmail.com>
2021-01-06 22:28:12 -0600

    rutas agregadas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,56 @@
 import './css/app.css';
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import { EventDashboard } from './events/eventDashboard/EventDashboard';
 import { Navbar } from './nav/Navbar';
+import { HomePage } from './home/HomePage';
+import { EventDetail } from './events-detail/EventDetail';
+import { EventForm } from './events/eventForm/EventForm';
+
+export interface EventData {
+  id: string;
+  title: string;
+  date: string;
+  category: string;
+  description: string;
+  city: string;
+  venue: string;
+  hostedBy: string;
+  hostPhotoURL: string;
+  attendees: EventAttendee[];
+}
+
+export interface EventAttendee {
+  id: string;
+  name: string;
+  photoURL: string;
+}
 
 const App = () => {
-  const [formOpen, setFormOpen] = useState(false);
-
   return (
-    <Fragment>
-      <Navbar setFormOpen={setFormOpen} />
-      <Container className='main'>
-        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
-      </Container>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Route exact path='/' component={HomePage} />
+        <Route
+          path={'/(.+)'}
+          render={() => (
+            <Fragment>
+              <Navbar />
+              <Container className='main'>
+                <Route exact path='/events' component={EventDashboard} />
+                <Route exact path='/events/:id' component={EventDetail} />
+                <Route
+                  exact
+                  path={['/createEvent', '/manage/:id']}
+                  component={EventForm}
+                />
+              </Container>
+            </Fragment>
+          )}
+        />
+      </Fragment>
+    </Router>
   );
 };
 
