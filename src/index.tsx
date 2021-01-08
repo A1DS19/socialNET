1f231175e66b6e635cd9cd5d11fb22b0ec163af1
A1DS19 <jp06171@gmail.com>
2021-01-07 21:33:38 -0600

    scroll to top added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { reducers } from './reducers/index';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ScrollToTop } from './scripts/scrollToTop';
 import { App } from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = composeWithDevTools({});
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <ScrollToTop />
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
