8b8f8fccab47824ae3c7baca55447c0a58621ca3
A1DS19 <jp06171@gmail.com>
2021-01-14 20:08:32 -0600

    modal actions

---
+++ src/actions/modals.ts
@@ -0,0 +1,28 @@
+import { types } from './types';
+
+export interface ModalPayload {
+  type: 'LoginForm';
+  props?: JSX.Element;
+}
+
+export interface OpenModalAction {
+  type: types.OPEN_MODAL;
+  payload: ModalPayload;
+}
+
+export interface CloseModalAction {
+  type: types.CLOSE_MODAL;
+}
+
+export const openModal = (payload: ModalPayload): OpenModalAction => {
+  return {
+    type: types.OPEN_MODAL,
+    payload,
+  };
+};
+
+export const closeModal = (): CloseModalAction => {
+  return {
+    type: types.CLOSE_MODAL,
+  };
+};
