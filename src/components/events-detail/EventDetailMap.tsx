855d282eaf8e4210a84cb8b844fb17978657d9b9
A1DS19 <jp06171@gmail.com>
2021-01-15 23:37:38 -0600

    mapa para mapa

---
+++ src/components/events-detail/EventDetailMap.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Coords } from '../../actions/event';
+import GoogleMapReact from 'google-map-react';
+import { Segment, Icon } from 'semantic-ui-react';
+
+interface MapProps {
+  center: Coords;
+  zoom: number;
+  heigth: number;
+  width: string;
+}
+
+export const Marker: React.FC<Coords> = ({ lat, lng }): JSX.Element => {
+  return <Icon name='marker' size='big' color='red' lat={lat} lng={lng} />;
+};
+
+const EventDetailMap: React.FC<MapProps> = ({
+  center,
+  zoom,
+  heigth,
+  width,
+}): JSX.Element => {
+  return (
+    <Segment attached='bottom' style={{ padding: 0 }}>
+      <div style={{ height: heigth, width: width }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCRFsufxDhfDWgeYaDQm-i0YZOOOxcw-aU' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        >
+          <Marker lat={center.lat} lng={center.lng} />
+        </GoogleMapReact>
+      </div>
+    </Segment>
+  );
+};
+
+export { EventDetailMap };
