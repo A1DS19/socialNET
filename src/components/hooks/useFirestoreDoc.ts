86753a21c5b881c9341884390405ba09efc9eff2
A1DS19 <jp06171@gmail.com>
2021-01-18 20:32:41 -0600

    hook useDoc

---
+++ src/components/hooks/useFirestoreDoc.ts
@@ -0,0 +1,37 @@
+import { dataFromSnapshot } from './../../firestore/firestoreService';
+import {
+  asyncActionStart,
+  asyncActionFinish,
+  asyncActionError,
+} from './../../actions/loading';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+interface Props {
+  query: any;
+  data: any;
+  dependencies: any;
+  shouldExecute?: boolean;
+}
+
+export function useFirestoreDoc({ query, data, dependencies, shouldExecute }: Props) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!shouldExecute) return;
+    dispatch(asyncActionStart());
+    const unsubcribe = query().onSnapshot(
+      (snapshot: any) => {
+        if (!snapshot.exists) {
+          dispatch(asyncActionError('No se encuentra el documento'));
+          return;
+        }
+        data(dataFromSnapshot(snapshot));
+        dispatch(asyncActionFinish());
+      },
+      (error: any) => dispatch(asyncActionError(error))
+    );
+    return () => {
+      unsubcribe();
+    };
+  }, dependencies); //eslint-disable-line react-hooks/exhaustive-deps
+}
